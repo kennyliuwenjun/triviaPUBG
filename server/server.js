7be12bfb0fe6253e10e3ef4541f23ce8c68068f5
Wenjun Kenny Liu <kennyliu1990110@hotmail.com>
2018-08-27 12:41:01 +1000

    socket.io basement

--- server/server.js
+++ server/server.js
@@ -1,12 +1,25 @@
-const path = require('path')
-const express = require('express')
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
 
 const clientPtah = path.join(__dirname,'../client');
 const port = process.env.PORT || 3000
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(express.static(clientPtah));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('disconnect', () => {
+    console.log('User is diconnected')
+  })
+
+});
+
+server.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 })
